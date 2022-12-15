a2c9a6c1ab0455d0c62a0b2c52c856ab07f63547
Ruben00alex <ruben.00alex@gmail.com>
2022-12-15 10:42:41 -0800

    Removed unnecesary

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,8 @@
 const Footer = () => {
 
     return (
-        //we need to place the footer on the bottom of the page,but we don't want to see it until we scroll down, so we use the sticky class
-        <footer className="w-full h-16 bg-slate-900 mt-auto flex flex-col justify-center items-center">
-            <p className="text-white text-center ">Site built by Alex</p>
+       <footer className="w-full h-16 bg-slate-900 mt-auto flex flex-col justify-center items-center">
+            <p className="text-white text-center ">Site built by Alex using tailwindcss and react</p>
             <a lassName="text-white text-center " href="https://github.com/Ruben00alex"> Github</a>
         </footer>
     )
